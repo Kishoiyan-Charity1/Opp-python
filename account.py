7b725cf30fbec01c2f0415f1b155fe4b04f05072
Kishoiyan-Charity1 <kishoiyancharity@gmail.com>
2022-06-02 21:53:29 +0300

    first commit

---
+++ account.py
@@ -0,0 +1,24 @@
+from curses import ALL_MOUSE_EVENTS
+from unicodedata import decomposition
+
+
+class Bank:
+    bank_name='kcb'
+    def __init__(self,name,number,type,amount):
+        self.name= name
+        self.number= number
+        self.type= type
+        self.amount= amount
+
+    def deposit(self,balance):
+        self.amount+=balance
+        return f'{self.amount}'
+
+    def withdraw(self,balance):
+        self.amount-=balance
+        return f'{self.amount}'
+
+        
+
+
+
---
+++ student.py
@@ -0,0 +1,30 @@
+class Student:
+    # name="kanini"
+    # age=20
+    # country="kenya"
+    # school="akirachix"
+    school="akirachix"
+    def __init__(self,first_name,second_name,age,country):
+        self.first_name= first_name
+        self.second_name= second_name
+        self.age= age
+        self.country= country
+
+
+    def greet(self):
+         return f'hello {self.name} from {self.country} welcome to {self.school}'
+
+    def full_name(self):
+        y= "f {self.first_name} {self.second_name}"
+        return y
+
+    def year_of_birth(self):
+        year_of_birth= 2000-self.age
+
+        return year_of_birth
+
+    def initials(self):
+        x= f'{self.first_name}{self.second_name}'
+        t=x.upper()
+        return t
+
