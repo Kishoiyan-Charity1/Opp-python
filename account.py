a91a8ee627b2795e6168c15b9e4e49c186ffe53f
Kishoiyan-Charity1 <kishoiyancharity@gmail.com>
2022-06-09 22:17:11 +0300

    first commit

--- account.py
+++ account.py
@@ -4,19 +4,51 @@
 
 class Bank:
     bank_name='kcb'
-    def __init__(self,name,number,type,amount):
+    def __init__(self,name,acc_number):
         self.name= name
-        self.number= number
-        self.type= type
-        self.amount= amount
+        self.balance= 3000
+        self.acc_number= acc_number
+        self.deposits=[] 
+        self.withdraws=[]
 
-    def deposit(self,balance):
-        self.amount+=balance
-        return f'{self.amount}'
 
-    def withdraw(self,balance):
-        self.amount-=balance
-        return f'{self.amount}'
+    def deposit(self,amount):
+        
+
+        
+        
+        if amount<=0:
+            return f'deposit should be more than zero'
+        else:
+            self.balance+=amount
+            self.deposits.append(amount)
+            return f'you have deposited {amount}, your new balance is {self.balance}'
+        
+        
+        
+
+    def withdraw(self,amount):
+        
+
+    
+        if amount > self.balance:
+            return f'your balance is {self.balance}, you cannot withdraw {amount}'
+
+        elif amount<=0:
+            return f'amont must be great than 0'
+
+        else:
+            self.balance-=amount
+            self.withdraws.append(amount)
+            return f'you have succeful withdrawed {amount}, your new balance is{self.balance}'
+
+      
+    def deposits_statement(self):
+         print(*self.deposits, sep='\n')
+
+    def withdraws_statement(self):
+        print(*self.withdraws, sep='\n')
+
 
         
 
--- student.py
+++ student.py
@@ -15,7 +15,7 @@ class Student:
          return f'hello {self.name} from {self.country} welcome to {self.school}'
 
     def full_name(self):
-        y= "f {self.first_name} {self.second_name}"
+        y= f"{self.first_name} {self.second_name}"
         return y
 
     def year_of_birth(self):
@@ -24,7 +24,7 @@ class Student:
         return year_of_birth
 
     def initials(self):
-        x= f'{self.first_name}{self.second_name}'
+        x= f'{self.first_name[0]}{self.second_name[0]}'
         t=x.upper()
         return t
 
